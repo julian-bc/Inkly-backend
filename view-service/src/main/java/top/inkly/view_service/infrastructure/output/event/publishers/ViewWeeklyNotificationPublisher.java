923b000389b0af5dfaf38cdd7d0b393d096e6137
Julian Enrique <julianbuitragocharry2006@gmail.com>
2026-04-30 15:50:07 -0500

    typos ViewPublisher

--- view-service/src/main/java/top/inkly/view_service/infrastructure/output/event/publishers/ViewWeeklyNotificationPublisher.java
+++ view-service/src/main/java/top/inkly/view_service/infrastructure/output/event/publishers/ViewWeeklyNotificationPublisher.java
@@ -5,15 +5,13 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 import top.inkly.shared.infrastructure.input.dto.notification.EmailNotificationRequestDTO;
 import top.inkly.shared.infrastructure.input.dto.notification.NotificationRequestDTO;
-import top.inkly.shared.infrastructure.output.queues.NotificationPublisherPort;
 import top.inkly.shared.infrastructure.output.queues.config.RabbitMQConfig;
 
 @Component
 @RequiredArgsConstructor
-public class VIewWeeklyNotificationPublisher implements NotificationPublisherPort {
+public class ViewWeeklyNotificationPublisher {
     private final RabbitTemplate rabbitTemplate;
 
-    @Override
     public void publishNotificationMessage(NotificationRequestDTO<EmailNotificationRequestDTO> requestDTO) {
         rabbitTemplate.convertAndSend(RabbitMQConfig.NOTIFICATION_QUEUE, requestDTO);
     }
