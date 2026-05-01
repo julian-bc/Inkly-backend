7fc8f9a14465459045250b52ffb15da0b0aa09b4
Julian Enrique <julianbuitragocharry2006@gmail.com>
2026-05-01 08:28:38 -0500

    feat: add OAS

--- view-service/src/main/java/top/inkly/view_service/ViewServiceApplication.java
+++ view-service/src/main/java/top/inkly/view_service/ViewServiceApplication.java
@@ -2,9 +2,11 @@ package top.inkly.view_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ViewServiceApplication {
 
