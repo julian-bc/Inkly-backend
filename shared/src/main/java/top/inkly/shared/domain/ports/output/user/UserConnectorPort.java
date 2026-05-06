16babcc2abea59cff4989ab6e1b6ae290830b43b
Julian Enrique <julianbuitragocharry2006@gmail.com>
2026-05-06 15:41:27 -0500

    add get user by id userConnectorPort

--- shared/src/main/java/top/inkly/shared/domain/ports/output/user/UserConnectorPort.java
+++ shared/src/main/java/top/inkly/shared/domain/ports/output/user/UserConnectorPort.java
@@ -1,7 +1,10 @@
 package top.inkly.shared.domain.ports.output.user;
 
+import top.inkly.shared.infrastructure.input.rest.dtos.user.UserResponse;
+
 import java.util.UUID;
 
 public interface UserConnectorPort {
+    UserResponse findUserById(UUID userId);
     void existsUserById(UUID userId);
 }
